17cb9fb3c65ea8d694cef3e854952c93791cd53f
tim@pythialabs.com <tim@pythialabs.com>
2025-12-07 04:29:58 +0000

    add constants

---
+++ src/constants.py
@@ -0,0 +1,5 @@
+PROMPT = "Can you solve the following math problem? "
+BASE = " Put your final answer within \\boxed{{}}."
+COT = " Please reason step by step, and put your final answer within \\boxed{{}}."
+COT_ALT = " Please explain your reasoning with a detailed, step-by-step solution, and present your final answer within \\boxed{{}}."
+GPQA_QUERY_TEMPLATE = "Answer the following multiple choice question. The last line of your response should be of the following format: '\\boxed{{$LETTER}}' (without quotes) where LETTER is one of ABCD (ex. '\\boxed{{A}}'). Think step by step before answering.\n\n{Question}\n\nA) {A}\nB) {B}\nC) {C}\nD) {D}"
